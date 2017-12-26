2ae3242bde88f3ba2a83a4f3044b066ec9ec669a
fabricio.barrozo <fabricio.barrozo@srh-3363.adasa.df.gov.br>
2017-12-26 17:46:06 -0200

    Sucesso na conexão do java com mysql.

--- ConexaoSQL/src/conexaosql/Conexao.java
+++ ConexaoSQL/src/conexaosql/Conexao.java
@@ -1,37 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package conexaosql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author fabmu
- */
 public class Conexao {
     
-    public Connection Connect() {
+    public static Connection ConexaoBanco (){
+        java.sql.Connection conexaoSQL = null;
+        
+        //chamar driver
+        String driver = "com.mysql.jdbc.Driver";
+        
+        // informações referente ao BD
+        String url = "jdbc:mysql://br234.hostgator.com.br:3306/somde078_EstudosMySQL";
+        String user = "somde078_conn";
+        String password  = "1.juetania";
+        
+        try {
+            Class.forName(driver);
+            conexaoSQL = DriverManager.getConnection(url, user, password);
+            return conexaoSQL;
+        } catch (ClassNotFoundException | SQLException e) {
+            return null;
+        }
+                 
+    }
+    public static void main(String[] args) {
+            Connection strConexao = Conexao.ConexaoBanco();
+            
+    }
+
+    /*
+    public static Connection Connect() {
+        /*
         try {
             String URL = "jdbc:mysql://br234.hostgator.com.br:2083/somde078_EstudosMySQL";
-            String USER = "somde078";
+            String USER = "somde078_conn";
             String PASSWORD = "1.juetania";
-        
+
             Class.forName("com.mysql.jdbc.Driver");
             Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+                    //.getConnection(URL, USER, PASSWORD);
             return conn;
-            
+
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
-        
-        //primeiro push
+    }*/
+
     
-}}
+    
+    /*
+    Ser for preciso para o mysql remoto, Meu IP: 200.186.56.188
+
+    
+    Informações sobre as configurações de DNS:
+
+    Nome do Servidor: br234.hostgator.com.br
+    IP do Servidor: 192.185.176.78
+
+    Se você estiver usando um domínio existente com a sua nova conta de hospedagem, 
+    você precisará atualizá-los para apontar para os DNS listados abaixo.
+
+    Nameserver 1: ns234.hostgator.com.br (192.185.176.78)
+    Nameserver 2: ns235.hostgator.com.br (192.185.217.16
+    */
+}
+   
